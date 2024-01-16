5e0e85ebdad3829df08e5f73cc75fad112d55b98
Martino Massalini <massalinimartino@gmail.com>
2024-01-16 21:34:26 +0100

    .

--- src/app/(dashboard)/SystemInfoCard.tsx
+++ src/app/(dashboard)/SystemInfoCard.tsx
@@ -2,6 +2,8 @@ import { Card, CardBody, CardHeader } from "@nextui-org/react"
 import LastUpdated from "@/app/(dashboard)/LastUpdated"
 import { getSystemInfo, isSystemUp } from "@/utils"
 
+export const dynamic = 'force-dynamic'
+
 export async function SystemInfoCard() {
   const systemInfo = await getSystemInfo()
   const systemUp = await isSystemUp()
@@ -12,6 +14,7 @@ export async function SystemInfoCard() {
         <div className="flex flex-col justify-between">
           <h1 className="text-2xl font-bold">Info Impianto</h1>
           {`${systemInfo.lastUpdateTime}`}
+          {`${new Date()}`}
           <LastUpdated date={systemInfo.lastUpdateTime} />
         </div>
       </CardHeader>
